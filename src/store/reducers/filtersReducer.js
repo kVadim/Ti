15d31bd9fb67a6df13e55df562a2f5f3d4cc3061
kVadim <1408vk@gmail.com>
2019-05-14 12:52:38 +0300

    filtersReduced refactoring

--- src/store/reducers/filtersReducer.js
+++ src/store/reducers/filtersReducer.js
@@ -10,22 +10,34 @@ export const filtersReducer = (state = initialState, action) => {
 	switch (action.type) {
 		// currentCatNameIndex
 		case 'APP_INCREASE_CAT_NAME_INDEX':
-			console.log('APP_INCREASE_CAT_NAME_INDEX');
-			return { app: { currentCatNameIndex: state.app.currentCatNameIndex + 1 } };
+			// console.log('APP_INCREASE_CAT_NAME_INDEX');
+			return {
+				...state,
+				app : { ...state.app, currentCatNameIndex: state.app.currentCatNameIndex + 1, currentCatItemIndex: 0 }
+			};
 		case 'APP_DECREASE_CAT_NAME_INDEX':
-			console.log('APP_DECREASE_CAT_NAME_INDEX');
-			return { app: { currentCatNameIndex: state.app.currentCatNameIndex - 1 } };
+			// console.log('APP_DECREASE_CAT_NAME_INDEX');
+			return {
+				...state,
+				app : { ...state.app, currentCatNameIndex: state.app.currentCatNameIndex - 1, currentCatItemIndex: 0 }
+			};
 		case 'APP_DECREASE_CAT_NAME_INDEX_ERROR':
 			console.log('APP_DECREASE_CAT_NAME_INDEX_ERROR', action.err);
 			return state;
 
 		// currentCatItemIndex
 		case 'APP_INCREASE_CAT_ITEM_INDEX':
-			console.log('APP_INCREASE_CAT_ITEM_INDEX');
-			return { app: { currentCatItemIndex: state.app.currentCatItemIndex + 1 } };
+			// console.log('APP_INCREASE_CAT_ITEM_INDEX');
+			return {
+				...state,
+				app : { ...state.app, currentCatItemIndex: state.app.currentCatItemIndex + 1 }
+			};
 		case 'APP_DECREASE_CAT_ITEM_INDEX':
-			console.log('APP_DECREASE_CAT_ITEM_INDEX');
-			return { app: { currentCatItemIndex: state.app.currentCatItemIndex - 1 } };
+			// console.log('APP_DECREASE_CAT_ITEM_INDEX');
+			return {
+				...state,
+				app : { ...state.app, currentCatItemIndex: state.app.currentCatItemIndex - 1 }
+			};
 		case 'APP_DECREASE_CAT_ITEM_INDEX_ERROR':
 			console.log('APP_DECREASE_CAT_ITEM_INDEX_ERROR', action.err);
 			return state;
