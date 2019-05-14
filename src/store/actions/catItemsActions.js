765bbf1ed676c44c5b930eb66de749f45032ce84
kVadim <1408vk@gmail.com>
2019-05-14 12:05:50 +0300

    catItemsActions.js

---
+++ src/store/actions/catItemsActions.js
@@ -0,0 +1,7 @@
+export const increaseCatItemIndexAction = () => ({
+	type : 'APP_INCREASE_CAT_ITEM_INDEX'
+});
+
+export const decreaseCatItemIndexAction = () => ({
+	type : 'APP_DECREASE_CAT_ITEM_INDEX'
+});
